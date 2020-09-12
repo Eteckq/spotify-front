b2f12a0d4b5439d4515859af15830afaadf96605
Yohan <ygastoud@altalis.fr>
2020-09-12 13:57:27 +0200

    start nuxt

---
+++ layouts/application.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
